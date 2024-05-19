1fed8ce1017046d099f39600a80e24c737a70ddc
moushow <1060047803@qq.com>
2024-05-19 15:38:10 +0800

    测试

--- springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,7 +2,10 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootApplication {
 
@@ -10,4 +13,8 @@ public class SpringbootApplication {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index(){
+        return "ok";
+    }
 }
